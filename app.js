087f8a33502312e07d63c619cc2c0c68365ee3fc
tabdurahmon <dilsozxonxatamova@gmail.com>
2024-04-24 22:25:08 +0500

    finished

---
+++ app.js
@@ -0,0 +1,123 @@
+// let number = +prompt("son kiriting");
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${number} x ${i} = ${number * i}`);
+// }
+
+// let text =
+//   "Lorem ipsum dolor sit amet conse!!ctetur adipi!sicing elit. Accusantium vel neque, architecto ipsam asperiores, impedit repellat quia aliquid consequuntur ab placeat distinctio molestiae quis. Accusamus, nisi. Rerum reprehenderit ullam provident?";
+
+// let counter = 0;
+// for (let i = 0; i < text.length; i++) {
+//   if (text[i].toLowerCase() == "!") {
+//     counter++;
+//     break;
+//   }
+// }
+
+// const number1 = +prompt(`birinchi raqamni kiriting`)
+// const belgi = prompt(`birinchi raqamni kiriting`)
+// const number1 = +prompt(`birinchi raqamni kiriting`)
+
+// switch
+
+// let text =
+//   "Lorem ipsum dolor sit amet conse!!ctetur adipi!sicing elit. Accusantium vel neque, architecto ipsam asperiores, impedit repellat quia aliquid consequuntur ab placeat distinctio molestiae quis. Accusamus, nisi. Rerum reprehenderit ullam provident?";
+
+// let newText = "";
+// let counter = 0;
+// for (let i = 0; i < text.length; i++) {
+//   if (text[i] != "!") {
+//     newText += text[i];
+//   }
+// }
+// console.log(newText);
+
+// Homework
+// for1
+
+// for4
+// let number = +prompt("1 kg konfet narxini kiriting");
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(number * i);
+// }
+
+// for5
+// let number = +prompt("1 kg konfet narxini kiriting");
+
+// for (let i = 0.1; i <= 1; i += 0.1) {
+//   console.log(number * i);
+// }
+
+// for6
+// let number = +prompt("1 kg konfet narxini kiriting");
+
+// for (let i = 1.2; i <= 2; i += 0.2) {
+//   console.log(number * i);
+// }
+
+// for7
+// function yigindi(A, B) {
+//   let sum = 0;
+//   for (let i = A; i <= B; i++) {
+//     sum += i;
+//   }
+//   return sum;
+// }
+
+// alert(yigindi(1, 20));
+
+// for8
+// function kopaytma(A, B) {
+//   let sum = 1;
+//   for (let i = A; i <= B; i++) {
+//     sum *= i;
+//   }
+//   return sum;
+// }
+
+// alert(yigindi(1, 20));
+
+// for9
+// function kvadrat(A, B) {
+//   let kvadrat = [];
+//   for (let i = A; i <= B; i++) {
+//     kvadrat.push(i * i);
+//   }
+//   return kvadrat;
+// }
+
+// alert(kvadrat(1, 5));
+
+// for10
+// function yigindi(n) {
+//   let sum = 0;
+//   for (let i = 1; i <= n; i++) {
+//     sum += 1 / i;
+//   }
+//   return sum;
+// }
+
+// alert(yigindi(6));
+
+// for11
+// function yigindi(n) {
+//   let sum = 0;
+//   for (let i = 1; i <= n; i++) {
+//     sum += (1 + i) ^ 2;
+//   }
+//   return sum;
+// }
+
+// alert(yigindi(6));
+
+// for12
+
+// let number1 = prompt("n son kiriting");
+
+// let number2 = 1;
+// for (let i = 1; i <= number1; i++) {
+//   number2 *= 1 + i * 0.1;
+// }
+// alert(number2);
